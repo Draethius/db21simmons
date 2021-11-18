c51489ac85d54975416e4cfae7cfb264408a9bf1
Draethius <54371536+Draethius@users.noreply.github.com>
2021-11-17 19:02:51 -0600

    Part 5, forgot to commit 3 and 4

--- controllers/tattoo.js
+++ controllers/tattoo.js
@@ -39,12 +39,35 @@ exports.tattoo_create_post = async function(req, res) {
 	}
 };
 
-exports.tattoo_delete = function(req, res) {
-    res.send('NOT IMPLEMENTED: Tattoo delete DELETE ' + req.params.id);
+exports.tattoo_delete = async function(req, res) {
+	console.log("delete " + req.params.id)
+	try{
+		result= await Tattoo.findByIdAndDelete(req.params.id)
+		console.log("Removed " + result)
+		res.send(result)
+	}
+	catch(err){
+		res.status(500)
+		res.send('{"error": Error deleting ${err}}');
+	}
 };
 
-exports.tattoo_update_put = function(req, res) {
-    res.send('NOT IMPLEMENTED: Tattoo update PUT' + req.params.id);
+exports.tattoo_update_put = async function(req,res){
+	    console.log('update on id ${req.params.id} with body ${JSON.stringify(req.body)}')
+	    try{
+		    let toUpdate = await Tattoo.findById(req.params.id)
+		    if(req.body.tatNum) toUpdate.tatNum = req.body.tatNum;
+		    if(req.body.tatColor) toUpdate.tatColor = req.body.tatColor;
+		    if(req.body.tatLocation) toUpdate.tatLocation = req.body.tatLocation;
+		    let result = await toUpdate.save();
+		    console.log("Success " + result)
+		    res.send(result)
+	    }
+	    catch(err){
+		    res.status(500)
+		    res.send('{"error": ${err}: Update for id ${req.params.id} failed');
+	    }
+	    
 };
 
 exports.tattoo_view_all_Page = async function(req, res) {
@@ -58,3 +81,26 @@ exports.tattoo_view_all_Page = async function(req, res) {
 	}
 };
 
+exports.tattoo_view_one_Page = async function(req,res){
+	console.log("single view for id " + req.query.id)
+	try{
+		result = await Tattoo.findById(req.query.id)
+		res.render('tattoodetail',
+			{ title: 'Tattoo Detail',toShow:result });
+	}
+	catch(err){
+		res.status(500)
+		res.send(`{'error': '${err}'}`);
+	}
+};
+
+exports.tattoo_create_Page = function(req,res){
+	console.log("create view")
+	try{
+		res.render('tattoocreate', {title: 'Tattoo Create'});
+	}
+	catch(err){
+		res.status(500)
+		res.send(`{'error': '${err}'}`);
+	}
+};
--- routes/tattoo.js
+++ routes/tattoo.js
@@ -6,4 +6,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', tattoo_controllers.tattoo_view_all_Page);
 
+router.get('/detail',tattoo_controllers.tattoo_view_one_Page);
+
+router.get('/create',tattoo_controllers.tattoo_create_Page);
+
 module.exports = router;
