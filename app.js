67a2a6ed4c1ef440a1a7a1e41601c8c0487856f6
Draethius <54371536+Draethius@users.noreply.github.com>
2021-12-01 17:30:35 -0600

    Added login features

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+	function(username,password,done){
+		Account.findOne({username: username}, function(err,user){
+			if(err){return done(err);}
+			if(!user){
+				return done(null,false,{message:'Incorrect username.'});
+			}
+			if(!user.validPassword(password)){
+				return done(null,false,{message:'Incorrect password.'});
+			}
+			return done(null,user);
+		});
+	}))
 
 var Tattoo = require("./models/tattoo");
 async function recreateDB(){
@@ -49,6 +64,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+	secret: 'keyboard cat',
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -58,20 +80,26 @@ app.use('/tattoo', tattooRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
 
+var Account = require('./models/account');
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
