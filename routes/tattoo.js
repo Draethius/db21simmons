d0ec8c95031bb87c1d6226a95ca74fac09a549e6
Draethius <54371536+Draethius@users.noreply.github.com>
2021-12-01 20:40:43 -0600

    Added authentication stuff

--- routes/tattoo.js
+++ routes/tattoo.js
@@ -2,6 +2,13 @@ var express = require('express');
 const tattoo_controllers=require('../controllers/tattoo');
 var router = express.Router();
 
+const secured = (req, res, next) => {
+	if(req.user){
+		return next();
+	}
+	req.session.returnTo = req.originalUrl;
+	res.redirect("/login");
+}
 
 /* GET home page. */
 router.get('/', tattoo_controllers.tattoo_view_all_Page);
@@ -10,7 +17,7 @@ router.get('/detail',tattoo_controllers.tattoo_view_one_Page);
 
 router.get('/create',tattoo_controllers.tattoo_create_Page);
 
-router.get('/update',tattoo_controllers.tattoo_update_Page);
+router.get('/update',secured,tattoo_controllers.tattoo_update_Page);
 
 router.get('/delete',tattoo_controllers.tattoo_delete_Page);
 
